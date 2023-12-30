a5e6fec7b0cea498dc644f738c32207cc912fbc6
m4rmen <90349436+m4rmen@users.noreply.github.com>
2023-12-29 20:16:44 -0500

    Client update

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,9 @@
 import React from "react";
+import Home from "./components/Home";
+import Host from "./components/Host";
+import Join from "./components/Join";
+import WaitingRoom from "./components/WaitingRoom";
+
 
 import { SocketProvider } from "./SocketContext";
 import { BrowserRouter as Router, Route } from "react-router-dom";
@@ -7,6 +12,9 @@ const App = () => {
     <SocketProvider>
         <Router>
             <Route exact path="/" component={Home} />
+            <Route exact path="/host" component={Host} />
+            <Route exact path="/join" component={Join} />
+            <Route exact path="/waitingroom" component={WaitingRoom} />
         </Router>
     </SocketProvider>
 }
